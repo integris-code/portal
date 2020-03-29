e6aefd1ada04686e950036b69ac3b2bee83af46b
Subhas Fagu <subhas85@gmail.com>
2020-03-28 22:09:37 -0400

    Installed vue-router & Got the menu working

--- distributor/src/main.js
+++ distributor/src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { BootstrapVue } from 'bootstrap-vue'
+import router from './router'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ distributor/src/views/Clients.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <Header title="Clients" :logo="logo"/>
+
+    <div class="container">
+      <div v-for="(client) in server.PipelineContactList" v-bind:key="client.id">
+        <b-card no-body class="mb-1" header-tag="header" align="left">
+          <b-card-header class="p-1" role="tab" align="left">
+            <b-button block href="#" align="left" variant="outline-default">
+              <i class="fe fe-help-circle mr-2" />
+              <span v-html="client.FirstName" />
+              <span class="ml-1" v-html="client.LastName" />
+            </b-button>
+          </b-card-header>
+        </b-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Header from '../components/Header.vue';
+
+export default {
+  name: "Clients",
+  props: {
+    logo: String
+  },
+  components:{
+      Header
+  },
+  data: function() {
+    return {
+      server: {
+        PipelineContactList: [
+          {
+            ClientCity: "",
+            ClientProvince: "",
+            CompanyName: "",
+            ContactGuid: "b1b16c8e-8520-4141-aee0-3ea7d5bfc257",
+            ContactStatus: "Inactive",
+            ContactStatusColor: "#EE2D24",
+            DisplayName: "Test1ttttt Test2",
+            FirstName: "Test1ttttt",
+            IllustrationRequested: true,
+            IllustrationUpdateId: null,
+            LastName: "Test2",
+            PrimaryEmailAddress: "testing@gmail.com",
+            PrimaryPhoneNumber: "4164564567",
+            Guid: "383aaabe-36cb-49a5-af1d-8e3949c74044",
+            Id: 209898,
+            SecurityPermissions: null,
+            TimeZone: null
+          }
+        ],
+        PipelineContactStatuss: ["Inactive"],
+        DisplayName: "Techlicity",
+        DistributorContactsContent: null,
+        DistributorPortal: "Techlicity",
+        DistributorPortalId: 153,
+        EnrolmentOverviewContent: null,
+        IntegrisContactsContent: null,
+        LandingView: "Techlicity",
+        LandingViewTitle: "Techlicity",
+        LanguageAbbreviation: null,
+        LanguageId: 0,
+        LanguageName: null,
+        LanguageNativeName: null,
+        PrimaryProductPlatform: null,
+        ProductPlatforms: null,
+        ResourceLibraryContent: null,
+        ResourceLibraryContentCategorys: null,
+        ResourceLibraryContentList: null,
+        UserEmailAddress: "farooq.kaiser@gmail.com",
+        UserFullName: "Kaiser, Farooq",
+        UserId: 2068,
+        WelcomeContent: null,
+        Guid: "7d46e6bd-1838-4f3e-865f-26ddbdbfd5b0",
+        Id: 153,
+        SecurityPermissions: null,
+        TimeZone: null
+      }
+    };
+  }
+};
+</script>
